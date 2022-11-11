4c4bf05ebdb11bddf7e0c6df71b5e60941c69ed3
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-11 16:45:10 +0200

    Ex 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Person = ({ person, number }) => {
+const Person = ({ person, number}) => {
   return (
     <div>{person} {number}</div>
   )
@@ -8,32 +8,43 @@ const Person = ({ person, number }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      id: 1, 
-      number: '046-1235346'
+    { 
+      name: 'Arto Hellas', number: '046-1235346', id: 1
+    }, 
+    {
+      name: 'Ada Lovelace', number: '05023454567', id: 2
+    }, 
+    {
+      name: 'Dan Abramov', number: '045-5467256', id: 3
+    },
+    {
+      name: 'Mary Poppendieck', number: '1234-435645', id: 4
     }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
     const isDuplicate = persons.some(person => person.name === newName)
 
-    if (isDuplicate === true && newName !== '') {
+    if (isDuplicate === true) {
       alert(`${newName} is already added to phonebook`)
       return
     }
 
-    const personObject = {
-      name: newName, 
-      id: persons.length + 1, 
-      number: newNumber
+    if (newName !== '') {
+      const personObject = {
+        name: newName, 
+        id: persons.length + 1, 
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -44,9 +55,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value.toLowerCase())
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+        value={newFilter}
+        onChange={handleFilterChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -66,8 +88,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-        <Person key={person.id} person={person.name} number={person.number} />
+        {persons.flatMap(person => person.name.toLowerCase().includes(newFilter) ? 
+        <Person 
+        key={person.id} 
+        person={person.name} 
+        number={person.number} 
+        /> :
+        ''
         )}
       </div>
     </div>
